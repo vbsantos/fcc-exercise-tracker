69e95fe68002038cf9c43e85e0c1b20eb4bf3107
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-03-04 19:15:50 -0300

    v3.3 - Fix Pipeline and tests

--- api/core/src/Controllers/App.Controller.ts
+++ api/core/src/Controllers/App.Controller.ts
@@ -94,7 +94,6 @@ export class AppController implements IAppController {
   }
 
   /**
-   * /api/users/:_id/exercises
    * A POST route for adding exercises to a user at /api/users/:_id/exercises that accepts form data with description, duration, and optionally date fields. If no date is supplied, the current date should be used. This route should add the exercise to the user and return the updated user object.
    */
   public async createExercise(
@@ -137,9 +136,8 @@ export class AppController implements IAppController {
   }
 
   /**
-   * /api/users/:_id/logs
-   * A GET route for retrieving a user's exercise log at /api/users/:_id/logs. This route should return a user object with a count property representing the number of exercises that belong to that user and a log array containing all the exercises. The log array should contain objects with description, duration, and date properties.
-   * /api/users/:_id/logs?from=&to=&limit=
+   * A GET route for retrieving a user's exercise log.
+   * This route should return a user object with a count property representing the number of exercises that belong to that user and a log array containing all the exercises. The log array should contain objects with description, duration, and date properties.
    * The GET route for retrieving a user's exercise log should also accept optional query parameters from, to, and limit. The from and to parameters should be dates in yyyy-mm-dd format and the limit parameter should be an integer. The route should return a subset of the user's exercise log based on the query parameters.
    */
   public async getUserExercises(
